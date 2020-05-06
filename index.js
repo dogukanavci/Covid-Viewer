91d65ee091ac3866f79a51d1da2ee139fea56939
dogukanavci <37452679+dogukanavci@users.noreply.github.com>
2020-05-06 16:51:30 +0400

    Init
    Init

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+router = express.Router();
+
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+
+//add the router
+app.use('/', router);
+
+app.listen(3000, function () {
+  console.log('Listening on port 3000');
+});
